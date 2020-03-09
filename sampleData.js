c6ed41350614b0e05b1ddf872c73c2a491074e6d
Jacob Silver <jacobsilver2@mac.com>
2020-03-09 10:21:04 -0400

    actual first

---
+++ sampleData.js
@@ -0,0 +1,290 @@
+export const motownData = [
+  {
+    title: {
+      title: "9 unidentified jazz instrumental tracks"
+    },
+    artists: [
+      {
+        artistName: "Benny Golson",
+        recordingInfo: {},
+        albums: []
+      }
+    ]
+  },
+  {
+    title: {
+      composer:
+        "Leonard Borisoff-Lamont Dozier-Brian Holland-Edward Holland Jr-John Madara-David Tricker",
+      title: "1 2 3",
+      publisher: "Jobete"
+    },
+    artists: [
+      {
+        artistName: "Len Barry",
+        recordingInfo: {},
+        albums: [
+          {
+            catalogNumber: "; Decca 31827 A",
+            format: "45 (M)",
+            releaseDate: "09-Sep-65",
+            info: "09-Sep-65; 45 (M): Decca 31827 A"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "spoken word",
+      title: "100 Years Later"
+    },
+    artists: [
+      {
+        artistName: "Rev. Martin Luther King",
+        recordingInfo: {
+          producer: "Lawrence Horn, Milton Henry",
+          location: "live Cobo Hall, Detroit,",
+          completed: "23-Jun-63"
+        },
+        albums: [
+          {
+            albumTitle: "The Great March To Freedom",
+            catalogNumber: "906",
+            recordLabel: "Gordy",
+            format: "LP (M)",
+            releaseDate: "Jun-63",
+            info: "Jun-63; LP (M): Gordy 906 The Great March To Freedom"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Harvey Fuqua-Thomas Kemp",
+      title: "12 O'Clock",
+      publisher: "Jobete",
+      publishedDate: "15-Sep-66"
+    },
+    artists: [
+      {
+        artistName: "The Spinners",
+        recordingInfo: {
+          producer: "Harvey Fuqua",
+          location: "Hitsville",
+          completed: "27-Mar-64"
+        },
+        albums: [
+          {
+            catalogNumber: "[pressing date unknown] [acetate]; Jobete",
+            format: "45 (M)",
+            info: "[pressing date unknown] [acetate]; 45 (M): Jobete"
+          },
+          {
+            albumTitle:
+              "Truly Yours ... Their First Motown Album With Bonus Tracks",
+            catalogNumber: "; CDTOP 371 [UK]",
+            recordLabel: "Kent",
+            format: "CD (M)",
+            releaseDate: "25-Jun-12",
+            info:
+              "25-Jun-12; CD (M): Kent CDTOP 371 Truly Yours ... Their First Motown Album With Bonus Tracks [UK]"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Bob West",
+      title: "2,000 Miles From Home",
+      publisher: "Stein & Van Stock",
+      publishedDate: "01-May-71"
+    },
+    artists: [
+      {
+        artistName: "Lodi",
+        recordingInfo: {
+          producer: "Bob West",
+          location: "MoWest"
+        },
+        albums: [
+          {
+            albumTitle: "Happiness",
+            catalogNumber: "MoWest MW101L",
+            format: "LP (S)",
+            releaseDate: "Oct-72",
+            info: "Oct-72; LP (S): MoWest MW101L Happiness"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Edwin Starr",
+      title: "24 Hours (To Find My Baby)",
+      publisher: "Jobete",
+      publishedDate: "31-Mar-69",
+      altTitle: "I Think You're The Greatest"
+    },
+    artists: [
+      {
+        artistName: "Edwin Starr",
+        recordingInfo: {
+          producer: "Edwin Starr",
+          location: "Hitsville-GW",
+          completed: "05-Mar-69"
+        },
+        albums: [
+          {
+            albumTitle: "25 Miles",
+            catalogNumber: "S940",
+            recordLabel: "Gordy",
+            format: "LP (S)",
+            releaseDate: "Apr-69",
+            info: "Apr-69; LP (S): Gordy S940 25 Miles"
+          },
+          {
+            albumTitle: "The Essential Collection",
+            catalogNumber: "; 544 492 2 [UK]",
+            recordLabel: "Spectrum",
+            format: "CD",
+            releaseDate: "26-Mar-01",
+            info:
+              "26-Mar-01; CD: Spectrum 544 492 2 The Essential Collection [UK]"
+          },
+          {
+            albumTitle: "Soul Master / 25 Miles",
+            catalogNumber: "016 839 2 [UK]",
+            recordLabel: "Motown",
+            format: "CD (S)",
+            releaseDate: "Oct-02",
+            info: "Oct-02; CD (S): Motown 016 839 2 Soul Master / 25 Miles [UK]"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Pamela Sawyer-Gloria Jones",
+      title: "2-4-6-8",
+      publisher: "Jobete",
+      publishedDate: "18-May-70"
+    },
+    artists: [
+      {
+        artistName: "The Jackson 5",
+        recordingInfo: {
+          producer: "Hal Davis",
+          location: "MoWest"
+        },
+        albums: [
+          {
+            albumTitle: "ABC",
+            catalogNumber: "MS709",
+            recordLabel: "Motown",
+            format: "LP (S)",
+            releaseDate: "May-70",
+            info: "May-70; LP (S): Motown MS709 ABC"
+          },
+          {
+            albumTitle: "Diana Ross Presents The Jackson 5 / ABC",
+            catalogNumber: "; 014 380 2",
+            recordLabel: "Motown",
+            format: "CD",
+            releaseDate: "07-Aug-01",
+            info:
+              "07-Aug-01; CD: Motown 014 380 2 Diana Ross Presents The Jackson 5 / ABC"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "B.B. King-Jules Taub",
+      title: "3 O'Clock Blues"
+    },
+    artists: [
+      {
+        artistName: "Love Sculpture",
+        recordingInfo: {
+          location: "Europe"
+        },
+        albums: [
+          {
+            albumTitle: "Blues Helping",
+            catalogNumber:
+              "S505 [first released on Parlophone (UK) PCS 7059 (1968)]",
+            recordLabel: "Rare Earth",
+            format: "LP (S)",
+            releaseDate: "Jun-69",
+            info:
+              "Jun-69; LP (S): Rare Earth S505 Blues Helping [first released on Parlophone (UK) PCS 7059 (1968)]"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Dennis Stoner",
+      title: "43rd Street",
+      publisher: "Jobete",
+      publishedDate: "28-Dec-71"
+    },
+    artists: [
+      {
+        artistName: "Dennis Stoner",
+        recordingInfo: {},
+        albums: [
+          {
+            albumTitle: "Dennis Stoner",
+            catalogNumber: "R530L",
+            recordLabel: "Rare Earth",
+            format: "LP (S)",
+            releaseDate: "Nov-71",
+            info: "Nov-71; LP (S): Rare Earth R530L Dennis Stoner"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: {
+      composer: "Jim Webb",
+      title: "5:30 Plane"
+    },
+    artists: [
+      {
+        artistName: "The Supremes",
+        recordingInfo: {
+          producer: "Jim Webb",
+          location: "MoWest"
+        },
+        albums: [
+          {
+            albumTitle: "The Supremes Produced And Arranged By Jimmy Webb",
+            catalogNumber: "M756L",
+            recordLabel: "Motown",
+            format: "LP (S)",
+            releaseDate: "Nov-72",
+            info:
+              "Nov-72; LP (S): Motown M756L The Supremes Produced And Arranged By Jimmy Webb"
+          },
+          {
+            albumTitle: "This Is The Story",
+            catalogNumber: "; B0005938-02",
+            recordLabel: "Hip-O Select",
+            format: "CD (S)",
+            releaseDate: "01-Dec-06",
+            info:
+              "01-Dec-06; CD (S): Hip-O Select B0005938-02 This Is The Story"
+          }
+        ]
+      }
+    ]
+  }
+];
